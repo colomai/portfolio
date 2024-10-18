7380de4a1980417ef99e702ded1a5a72cd63887d
jinmu <user@jinmuMacBook-Air.local>
2024-10-18 15:47:17 +0900

    インデント

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,20 +1,20 @@
 class ShiftsController < ApplicationController
-    def new
-        @shift = Shift.new
-    end 
+  def new
+    @shift = Shift.new
+  end 
 
-    def create
-        @shift = Shift.new(shift_params)
-        if @shift.save
-          redirect_to employees_top_path, notice: "シフト申請しました"
-        else
-          render :new
-        end
-      end
-    
-      private
-    
-      def shift_params
-        params.require(:shift).permit(:date, :start_time, :end_time, :user_id)
-      end
+  def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+        redirect_to employees_top_path, notice: "シフト申請しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:date, :start_time, :end_time, :user_id)
+  end
 end
