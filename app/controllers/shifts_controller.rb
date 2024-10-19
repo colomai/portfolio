3b8d579a5fb8a4776e9c558bda6f1087ff855207
jinmu <user@jinmuMacBook-Air.local>
2024-10-19 11:53:38 +0900

    シフト状況ページ変更

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,25 +1,26 @@
 class ShiftsController < ApplicationController
-  before_action :authenticate_user!, except: [:status]
+  before_action :authenticate_user!, except: [:index]
     def new
         @shift = Shift.new
     end
 
     def create
-        @shift = Shift.new(shift_params)
-        if @shift.save
-          redirect_to users_employees_top_path, notice: "シフト申請しました"
-        else
-          render :new
-        end
+      @shift = Shift.new(shift_params)
+      if @shift.save
+        redirect_to employees_top_path, notice: "シフト申請しました"
+      else
+        render :new
       end
+    end
 
-    def status
-        @shift = Shift.find_by(date: params[:date])
+    def index
+      date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      @shift = Shift.find_by(date: date)
     end
     
-      private
+    private
     
-      def shift_params
-        params.require(:shift).permit(:date, :start_time, :end_time)
-      end
+    def shift_params
+      params.require(:shift).permit(:date, :start_time, :end_time)
+    end
 end
