45e410ebbe0356aee09931ebc1a28bb1a8b06bbf
jinmu <user@jinmuMacBook-Air.local>
2024-10-19 12:57:46 +0900

    createメソッド修正

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,27 +1,26 @@
 class ShiftsController < ApplicationController
   before_action :authenticate_user!, except: [:status]
     def new
-        @shift = Shift.new
+      @shift = Shift.new
     end
 
     def create
-        @shift = Shift.new(shift_params)
-        @shift.user_id = current_user.id
-        @shift.status = "pending"
-        if @shift.save
-          redirect_to employees_top_path, notice: "シフト申請しました"
-        else
-          render :new
+      @shift = current_user.shifts.new(shift_params)
+      @shift.status = "pending"
+      if @shift.save
+        redirect_to employees_top_path, notice: "シフト申請しました"
+      else
+        render :new
         end
-      end
+    end
 
     def status
-        @shift = Shift.find_by(date: params[:date])
+      @shift = Shift.find_by(date: params[:date])
     end
     
-      private
+    private
     
-      def shift_params
-        params.require(:shift).permit(:date, :start_time, :end_time)
-      end
+    def shift_params
+      params.require(:shift).permit(:date, :start_time, :end_time)
+    end
 end
