6b694957072a19036440ad2e505b2d5da5e70bda
jinmu <user@jinmuMacBook-Air.local>
2024-10-25 12:26:15 +0900

    リクエストのコードに合わせただけ

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,27 +1,27 @@
 class ShiftsController < ApplicationController
-  before_action :authenticate_user!, except: [:status]
-    def new
-        @shift = Shift.new
-    end
+  before_action :authenticate_user!, except: [:index]
+  def new
+    @shift = Shift.new
+  end
 
-    def create
-        @shift = Shift.new(shift_params)
-        @shift.user_id = current_user.id
-        @shift.status = "pending"
-        if @shift.save
-          redirect_to employees_top_path, notice: "シフト申請しました"
-        else
-          render :new
-        end
-      end
-
-    def status
-        @shift = Shift.find_by(date: params[:date])
+  def create
+    @shift = current_user.shifts.new(shift_params)
+    @shift.status = "pending"
+    if @shift.save
+      redirect_to employees_top_path, notice: "シフト申請しました"
+    else
+      render :new
     end
+  end
+
+  def index
+    date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    @shift = Shift.find_by(date: params[:date])
+  end
     
-      private
+  private
     
-      def shift_params
-        params.require(:shift).permit(:date, :start_time, :end_time)
-      end
+  def shift_params
+    params.require(:shift).permit(:date, :start_time, :end_time)
+  end
 end
