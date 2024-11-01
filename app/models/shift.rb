dda830f861adfccb9937685db7ba00b0c24681fb
jinmu <user@jinmuMacBook-Air.local>
2024-11-01 18:56:34 +0900

    サーバーサイドバリデーション

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,8 @@ class Shift < ApplicationRecord
   belongs_to :user
   has_one :absent, dependent: :destroy  # 一対一の関係
   has_many :notifications, dependent: :destroy
+
+  validates :date, presence: true, format: { with: /\A\d{4}-\d{1,2}-\d{1,2}\z/ }, date: { after_or_equal_to: Date.today }
+  validates :start_time, presence: true, format: { with: /\A\d{2}:\d{2}\z/ }, time: { after: '00:00', before: '24:00' }
+  validates :end_time, presence: true, format: { with: /\A\d{2}:\d{2}\z/ }, time: { after: :start_time, before: '24:00' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :shifts, dependent: :destroy
   has_many :absents, dependent: :destroy
   has_many :notifications, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-z\d\w]{8,}\z/ }
 end
