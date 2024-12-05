94e36cc16bbbbafb5525a49a0d64fb637bea7608
jinmu <user@jinmuMacBook-Air.local>
2024-12-05 22:58:34 +0900

    インデックス追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_05_010101) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_05_135653) do
   create_table "absents", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "shift_id", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_05_010101) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
