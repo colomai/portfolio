9e50e6eb5a47daa1584cf798285ae332d1cc0ab5
jinmu <user@jinmuMacBook-Air.local>
2024-12-11 19:46:39 +0900

    従業員マイページ

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,6 @@
 class EmployeesController < ApplicationController
+  before_action :authenticate_user!
+
   def top      
     @year = params[:year].present? ? params[:year] .to_i : Date.today.year
     @month = params[:month].present? ? params[:month].to_i : Date.today.month
@@ -7,5 +9,26 @@ class EmployeesController < ApplicationController
     @first_day = Date.new(@year, @month, 1)
     @last_day = @first_day.end_of_month
     @first_day_wday = @first_day.wday
+  end
+
+  def show
+    @user = current_user
+  end
+  def edit
+    @user = current_user
+  end
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to employee_path(@user), notice: 'プロフィールを更新しました'
+    else
+      render :edit, alert: '更新に失敗しました'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image)
   end
 end
