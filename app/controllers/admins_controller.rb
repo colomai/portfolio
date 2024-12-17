aacec4c9914d524d3a88d48e6ceee033ac53d44f
colomai <lilcryptotrapstar@gmail.com>
2024-12-17 13:14:08 +0900

    申請一覧ページ仮

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,6 +4,12 @@ class AdminsController < ApplicationController
   def top
   end
     def shifts
-      @shifts = Shift.all
+      @user = User.find_by(number: params[:employee_number])
+      if @user.present?
+        @shifts = @user.shifts.order(start_time: :desc).page(params[:page])
+      else
+        flash[:alert] = '指定された社員番号のユーザーは存在しません'
+        redirect_to admin_shifts_path
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get 'employees/top', to: 'employees#top'
+  resources :employees
   get 'admins/top', to: 'admins#top'
   root "static_pages#home"
   get 'admins/shifts', to: 'admins#shifts', as: :admin_shifts
