329c84aaa7c32ee00644b19a65ee461ee145940a
colomai <lilcryptotrapstar@gmail.com>
2024-12-24 15:22:18 +0900

    シフト一覧ページ実装

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -1,12 +1,13 @@
 class Admin::ShiftsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @user = User.find_by(number: params[:employee_number])
+    if params[:number].present?
+      @user = User.find_by(number: params[:number])
       if @user.present?
-        @shifts = @user.shifts.order(start_time: :desc).page(params[:page])
+        @shifts = @user.shifts.order(start_time: :desc).page(params[:page]).per(5)
       else
         flash[:alert] = '指定された社員番号のユーザーは存在しません'
-        redirect_to admin_shifts_path
       end
+    end
   end
 end
