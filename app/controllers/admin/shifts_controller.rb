8dda6745ff325fe24549cd6fb67d97dfa460639d
colomai <lilcryptotrapstar@gmail.com>
2024-12-25 22:48:37 +0900

    承認、却下機能

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -10,4 +10,16 @@ class Admin::ShiftsController < ApplicationController
       end
     end
   end
+  def update
+    @shift = Shift.find(params[:id])
+      if @shift.update(shift_params) # statusの更新
+        redirect_to admin_shifts_path(number: params[:number])
+      else
+        redirect_to admin_shifts_path(number: params[:number]), alert: "シフトの更新に失敗しました。"
+      end
+  end
+  private
+  def shift_params
+    params.require(:shift).permit(:status)
+  end
 end
