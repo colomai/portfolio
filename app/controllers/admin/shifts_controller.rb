17015a974d16630f13986ceafd15efde55b7825a
colomai <lilcryptotrapstar@gmail.com>
2024-12-26 20:25:41 +0900

    承認却下機能修正

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -3,23 +3,25 @@ class Admin::ShiftsController < ApplicationController
   def index
     if params[:number].present?
       @user = User.find_by(number: params[:number])
-      if @user.present?
-        @shifts = @user.shifts.order(start_time: :desc).page(params[:page]).per(5)
-      else
-        flash[:alert] = '指定された社員番号のユーザーは存在しません'
-      end
+        if @user.present?
+          @shifts = @user.shifts.order(start_time: :desc).page(params[:page]).per(5)
+        else
+          flash[:alert] = '指定された社員番号のユーザーは存在しません'
+        end
     end
   end
+
   def update
     @shift = Shift.find(params[:id])
-      if @shift.update(shift_params) # statusの更新
+      if @shift.update(shift_update_params) # statusの更新
         redirect_to admin_shifts_path(number: params[:number])
       else
         redirect_to admin_shifts_path(number: params[:number]), alert: "シフトの更新に失敗しました。"
       end
   end
+
   private
-  def shift_params
+  def shift_update_params
     params.require(:shift).permit(:status)
   end
 end
