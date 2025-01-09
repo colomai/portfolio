b3741b4bb9c63b3ddb0ce48fea39a4ee8177767e
colomai <lilcryptotrapstar@gmail.com>
2025-01-09 13:16:47 +0900

    管理者マイページ修正

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,9 +7,11 @@ class AdminsController < ApplicationController
   def show
     @user = current_user
   end
+
   def edit
     @user = current_user
   end
+
   def update
     @user = current_user
     if @user.update(user_params)
